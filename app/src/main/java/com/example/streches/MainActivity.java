47df102c53d4eb7fb35c589d7f9de337c3310586
cshank19 <cshank19@users.noreply.github.com>
2020-08-31 16:27:41 -0400

    Initial commit

---
+++ app/src/main/java/com/example/streches/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.streches;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // is this shit working?
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
